5151ce9c30604cd6c59380507e3dbf3c872d3d8f
doboo <38196962@qq.com>
2015-08-01 11:11:35 +0800

    xi

--- alex.sh
+++ alex.sh
@@ -14,7 +14,7 @@ echo "/usr/bin/lxsession -s LXDE &" >> ~/.vnc/xstartup
 #把地址保存到一个文件
  echo "https://www.alexamaster.net/autosurf.php?Master=20648" >> ~/alexurl
 
-nano ~/.vnc/xstartup
+#nano ~/.vnc/xstartup
 tightvncserver :1
 #安装firefox
 wget ftp://ftp.mozilla.org/pub/firefox/releases/32.0/linux-i686/en-US/firefox-32.0.tar.bz2
@@ -28,7 +28,7 @@ wget http://vagex-debian.googlecode.com/files/libflashplayer.so
 mkdir -p /usr/local/lib/firefox/plugins
 mv libflashplayer.so /usr/local/lib/firefox/plugins/
 #支持中文语言
-apt-get install ttf-arphic-ukai ttf-arphic-uming ttf-arphic-gbsn00lp ttf-arphic-bkai00mp ttf-arphic-bsmi00lp
+apt-get install ttf-arphic-ukai ttf-arphic-uming ttf-arphic-gbsn00lp ttf-arphic-bkai00mp ttf-arphic-bsmi00lp -y
 
 
 #创建重启脚本
@@ -36,8 +36,8 @@ cat <<- EOF >> /root/alex/myrestart.sh
 #!/bin/sh
 export DISPLAY=localhost:1.0
 rm -rf ~/.vnc/*.log /tmp/plugtmp* > /dev/null 2>&1
-killall firefox  > /dev/null 2>&1
-/usr/bin/firefox > /dev/null 2>&1 &
+killall  iceweasel  > /dev/null 2>&1
+/usr/bin/iceweasel > /dev/null 2>&1 &
 EOF
 #给予启动权限
 chmod a+x /root/myrestart.sh
@@ -46,3 +46,8 @@ echo "0 * * * * root /root/alex/myrestart.sh" >>/etc/crontab
 crontab -l
 #重启定时器
 /etc/init.d/cron restart
+#设置VNC随系统启动
+	#删除exit 0
+   sed -i '/exit/d' /etc/rc.local
+echo "tightvncserver :1" >> /etc/rc.local
+echo "/usr/bin/iceweasel > /dev/null 2>&1 &" >> /etc/rc.local
