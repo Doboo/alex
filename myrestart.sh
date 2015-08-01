519f779db53614fa4795aa62e4d30118f9565cbc
doboo <38196962@qq.com>
2015-08-01 14:07:02 +0800

    初稿完成

--- myrestart.sh
+++ myrestart.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 export DISPLAY=localhost:1.0
 rm -rf ~/.vnc/*.log /tmp/plugtmp* > /dev/null 2>&1
-killall firefox  > /dev/null 2>&1
-/usr/bin/firefox > /dev/null 2>&1 &
+killall iceweasel 
+nohup iceweasel > /dev/null 2>&1 &
